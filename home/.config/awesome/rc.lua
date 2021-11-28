6ccf67e71b95161eb716191237fc7ef8fcd497d5
Hannah Lappe <hannah@lappe.page>
2021-11-28 16:31:17 +0100

    auto update

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -155,7 +155,7 @@ awful.rules.rules = {
         },
         properties = {
             maximized = false,
-            opacity = 1,
+            -- opacity = 0.2,
         }
     },
 
@@ -331,6 +331,12 @@ client.connect_signal("mouse::enter", function(c)
     c:emit_signal("request::activate", "mouse_enter", {raise = false})
 end)
 
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+client.connect_signal("focus", function(c) 
+    c.border_color = beautiful.border_focus 
+    c.opacity = 1
+end)
+client.connect_signal("unfocus", function(c) 
+    c.border_color = beautiful.border_normal 
+    c.opacity = 0.7
+end)
 -- }}}
