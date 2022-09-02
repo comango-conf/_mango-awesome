7280bdb9b64c13864cd7855e7817887b45aa2db4
Hannah Lappe <hannah@lappe.page>
2022-09-02 18:42:38 +0200

    auto update

---
+++ nix/modules/awesome.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+
+{
+
+  nixpkgs.overlays = [ (self: super: {
+    awesome = (super.awesome.override {
+      lua = self.lua5_3;
+    }).overrideAttrs ( old: {
+      name = "awesome-git";
+      src = super.fetchFromGitHub {
+        owner = "awesomeWM";
+        repo = "awesome";
+        rev = "05a405b38bbcb8fa3b344d45d94d4f56b83c74df";
+        sha256 = "OBCUbkWEcWHokYNjfz4aRRkxr9rwGNkaKnovzoliFwU=";
+      };
+    });
+  })];
+
+   
+  services.xserver.windowManager.awesome = {
+    luaModules = with pkgs.luaPackages; [
+      #luarocks
+    ];
+  };
+
+
+  environment.systemPackages = with pkgs; [
+    pavucontrol
+    xclip
+    imagemagick
+    pamixer
+  ];
+}
